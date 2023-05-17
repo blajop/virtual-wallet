6fa604457d866e268482489f61a570d6be072e0a
Stanislav Milchev <stanislavmilchev.stz@gmail.com>
2023-05-17 17:05:10 +0300

    Snowflake ID documented

--- app/helpers/snowflake_ids.py
+++ app/helpers/snowflake_ids.py
@@ -8,11 +8,17 @@ def generate_id(instance: int = 1) -> str:
     Args:
         instance: int between 0 and 1023; the seed for generation
     Returns:
-        19 digits in a str format
+        str: 19 digits in a str format
     """
     return str(next(SnowflakeGenerator(instance)))
 
 
 def datetime_from_id(id: str) -> datetime:
-    """Parses a snowflake ID and retrieves the datetime from it in UTC"""
+    """Parses a snowflake ID and retrieves the datetime from it in UTC
+
+    Args:
+        id: str
+    Returns:
+        datetime: when the ID was issued
+    """
     return Snowflake.parse(int(id)).datetime
