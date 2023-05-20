726d216f1784cb3145512fc052144fbfaa31009a
Stanislav Milchev <stanislavmilchev.stz@gmail.com>
2023-05-20 14:21:04 +0300

    DB session

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 from app.core.config import settings
 
 engine = create_engine(settings.DB_ENGINE_URI, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
