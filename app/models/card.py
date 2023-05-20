7b8f3c9788cd08d004c5b272befd6d6bd7aa819c
blajop <pavlov_blago@yahoo.com>
2023-05-20 12:13:29 +0300

    update card model with base

--- app/models/card.py
+++ app/models/card.py
@@ -9,24 +9,24 @@ from pydantic import constr, BaseModel
 from sqlmodel import Field, Relationship, SQLModel
 
 
-class CardRegister(BaseModel):
-    number: constr(regex="^\d{16}$") = Field(unique=True)
-    expiry: datetime
-    holder: constr(min_length=2, max_length=30)
-    cvc: constr(regex="^\d{3}$")
-
-
 class UserCardLink(SQLModel, table=True):
     __tablename__ = "cards_users"
     user_id: str = Field(foreign_key="users.id", primary_key=True)
     card_id: str = Field(foreign_key="cards.id", primary_key=True)
 
 
-class Card(SQLModel, table=True):
-    __tablename__ = "cards"
-    id: Optional[str] = Field(primary_key=True)
+class CardBase(SQLModel):
     number: constr(regex="^\d{16}$") = Field(unique=True)
     expiry: datetime
     holder: constr(min_length=2, max_length=30)
     cvc: constr(regex="^\d{3}$")
+
+
+class Card(CardBase, table=True):
+    __tablename__ = "cards"
+    id: Optional[str] = Field(primary_key=True)
     users: User = Relationship(back_populates="cards", link_model=UserCardLink)
+
+
+class CardCreate(CardBase):
+    pass
