24a72c5971c34544d963a39c59d64898d62c5a30
Stanislav Milchev <stanislavmilchev.stz@gmail.com>
2023-05-20 19:58:45 +0300

    Add get_user dependancy

--- app/api/deps.py
+++ app/api/deps.py
@@ -4,12 +4,13 @@ from jose.exceptions import JWTError
 from pydantic import ValidationError
 from sqlmodel import Session
 from app.core import security, security_copy
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, Path, status
 from app.core.config import settings
 from app.db.session import engine
 from fastapi.security import OAuth2PasswordBearer
 from app import crud
 from app import models
+from app.models.user import User
 
 
 reusable_oauth2 = OAuth2PasswordBearer(
@@ -21,6 +22,16 @@ reusable_oauth2 = OAuth2PasswordBearer(
 def get_db():
     with Session(engine) as session:
         yield session
+
+
+def get_user_from_path(identifier: str = Path(), db: Session = Depends(get_db)):
+    user: User = crud.user.get(db, identifier)
+
+    if not user:
+        raise HTTPException(
+            status_code=404, detail="User with such identifier does not exist!"
+        )
+    return user
 
 
 def get_current_user(
