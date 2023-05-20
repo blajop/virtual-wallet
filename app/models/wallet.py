a46b1c4f464438d68c5d3b1e629da8612da17727
blajop <pavlov_blago@yahoo.com>
2023-05-20 12:16:30 +0300

    update wallet model with base

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -8,25 +8,28 @@ from sqlmodel import Field, Relationship, SQLModel
 from pydantic import BaseModel, constr
 
 
-class WalletCreate(BaseModel):
-    currency: constr(regex="^(USD|EUR|BGN|CAD|AUD|CHF|CNY|JPY|GBP|NOK)$")
-
-
 class UserWalletLink(SQLModel, table=True):
     __tablename__ = "shared_wallets"
     user_id: str = Field(foreign_key="users.id", primary_key=True)
     wallet_id: str = Field(foreign_key="wallets.id", primary_key=True)
 
 
-class Wallet(SQLModel, table=True):
-    __tablename__ = "wallets"
-    id: Optional[str] = Field(primary_key=True)
+class WalletBase(SQLModel):
     owner_id: Optional[str] = Field(default=None, foreign_key="users.id")
     currency: constr(regex="^(USD|EUR|BGN|CAD|AUD|CHF|CNY|JPY|GBP|NOK)$")  # Currency
     balance: float = Field(default=0)
+
+
+class Wallet(WalletBase, table=True):
+    __tablename__ = "wallets"
+    id: Optional[str] = Field(primary_key=True)
 
     users: User = Relationship(back_populates="wallets", link_model=UserWalletLink)
 
     owner: User = Relationship(
         sa_relationship_kwargs=dict(primaryjoin="User.id==Wallet.owner_id")
     )  # lazy="joined" can be added here
+
+
+class WalletCreate(SQLModel):
+    currency: constr(regex="^(USD|EUR|BGN|CAD|AUD|CHF|CNY|JPY|GBP|NOK)$")
