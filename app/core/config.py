c6d6e5eed82a1bd0ba5f7fdf367ac8baa9177dc2
Stanislav Milchev <stanislavmilchev.stz@gmail.com>
2023-05-21 16:54:56 +0300

    Update Settings

--- app/core/config.py
+++ app/core/config.py
@@ -3,28 +3,30 @@ from pydantic import AnyHttpUrl, BaseSettings, EmailStr, Field
 
 
 class Settings(BaseSettings):
+    PROJECT_NAME: str = "Kache"
+
     API_V1_STR: str = "/api/v1"
+
     SECRET_KEY: str = Field(default="secret", env="SECRET_KEY")
     ALGORITHM: str = "HS256"
-    # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
 
     # SERVER_HOST: str = "http://91.139.226.224"
     SERVER_HOST: str = "http://localhost:8000"
     DB_ENGINE_URI: str = Field(default="", env="DB_ENGINE")
 
-    PROJECT_NAME: str = "Kache"
-
+    # EMAIL SETTINGS
     SMTP_PORT: Optional[int] = 465
     SMTP_HOST: Optional[str] = "smtp.abv.bg"
     SMTP_USER: Optional[str] = Field(default="", env="SMTP_LOGIN_MAIL")
     SMTP_PASSWORD: Optional[str] = Field(default="", env="SMTP_LOGIN_PASS")
+
     EMAILS_FROM_EMAIL: Optional[EmailStr] = Field(default="", env="SMTP_LOGIN_MAIL")
     EMAILS_FROM_NAME: Optional[str] = PROJECT_NAME
-
     EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
     EMAIL_TEMPLATES_DIR: str = "app/email-templates/build"
 
+    # APIs
     CURRENCY_REQUEST_URL = Field(default="", env="CURRENCY_REQUEST_URL")
 
     # TESTING
