0227094c1fe3b7c09241f3f1a199e3401dd51a85
blajop <pavlov_blago@yahoo.com>
2023-05-24 16:00:14 +0300

    working tests with scope='function' setting

--- app/tests/api/api_v1/test_cards.py
+++ app/tests/api/api_v1/test_cards.py
@@ -9,20 +9,22 @@ from datetime import datetime, timedelta
 from app import utils
 from fastapi.encoders import jsonable_encoder
 from app import deps
-from app.tests.utils.utils import random_user
+from app.tests.utils.utils import random_user, random_card
 from main import app
 
 from app.models.user import User
 
 
-curr_year = datetime.now().year
+@pytest.fixture(name="card")
+def card_fixture(session: Session):
+    curr_year = datetime.now().year
+    yield CardCreate(
+        number="1234567890123456",
+        expiry=jsonable_encoder(CardExpiry(mm="09", yyyy=str(curr_year + 1))),
+        holder="User Userov",
+        cvc="345",
+    )
 
-card_1 = CardCreate(
-    number="1234567890123456",
-    expiry=jsonable_encoder(CardExpiry(mm="09", yyyy=str(curr_year + 1))),
-    holder="User Userov",
-    cvc="345",
-)
 
 # card_2 = CardCreate(
 #     number=6543210987654321,
@@ -34,21 +36,25 @@ card_1 = CardCreate(
 # TEST POST ------------------------------------
 
 
-def test_add_card_succeeds_when_userAndValidData(client: TestClient, user):
+def test_add_card_succeeds_when_userAndValidData(
+    client: TestClient, user, card: CardCreate
+):
     app.dependency_overrides[deps.get_current_user] = user
 
-    response = client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    response = client.post("/api/v1/cards", json=jsonable_encoder(card))
     data = response.json()
 
     assert response.status_code == 200
-    assert data["number"] == card_1.number
+    assert data["number"] == card.number
     app.dependency_overrides.clear()
 
 
-def test_add_card_raises401_when_NotLoggedUser(client: TestClient, guest):
+def test_add_card_raises401_when_NotLoggedUser(
+    client: TestClient, guest, card: CardCreate
+):
     app.dependency_overrides[deps.get_current_user] = guest
 
-    response = client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    response = client.post("/api/v1/cards", json=jsonable_encoder(card))
     data = response.json()
 
     assert response.status_code == 401
@@ -57,29 +63,31 @@ def test_add_card_raises401_when_NotLoggedUser(client: TestClient, guest):
     app.dependency_overrides.clear()
 
 
-def test_add_card_raises400_when_sameCardNumberWithDiffData(client: TestClient, user):
+def test_add_card_raises400_when_sameCardNumberWithDiffData(
+    client: TestClient, user, card: CardCreate
+):
     app.dependency_overrides[deps.get_current_user] = user
 
-    client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    client.post("/api/v1/cards", json=jsonable_encoder(card))
 
-    card_1.cvc = "111"
+    card.cvc = "111"
 
-    response = client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    response = client.post("/api/v1/cards", json=jsonable_encoder(card))
     data = response.json()
 
     assert response.status_code == 400
     assert data["detail"] == "This card # already exists with different credentials"
-
-    card_1.cvc = "345"
     app.dependency_overrides.clear()
 
 
-def test_add_card_raises400_when_userAlreadyHasTheCard(client: TestClient, user):
+def test_add_card_raises400_when_userAlreadyHasTheCard(
+    client: TestClient, user, card: CardCreate
+):
     app.dependency_overrides[deps.get_current_user] = user
 
-    client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    client.post("/api/v1/cards", json=jsonable_encoder(card))
 
-    response = client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    response = client.post("/api/v1/cards", json=jsonable_encoder(card))
     data = response.json()
 
     assert response.status_code == 400
@@ -87,38 +95,39 @@ def test_add_card_raises400_when_userAlreadyHasTheCard(client: TestClient, user)
     app.dependency_overrides.clear()
 
 
-def test_add_card_raises400_when_cardIsExpired(client: TestClient, user):
+def test_add_card_raises400_when_cardIsExpired(
+    client: TestClient, user, card: CardCreate
+):
+    curr_year = datetime.now().year
     app.dependency_overrides[deps.get_current_user] = user
 
-    card_1.expiry = jsonable_encoder(CardExpiry(mm="09", yyyy=str(curr_year - 1)))
+    card.expiry = jsonable_encoder(CardExpiry(mm="09", yyyy=str(curr_year - 1)))
 
-    response = client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    response = client.post("/api/v1/cards", json=jsonable_encoder(card))
     data = response.json()
 
     assert response.status_code == 400
     assert data["detail"] == "Your card is expired"
-
-    card_1.expiry = jsonable_encoder(CardExpiry(mm="09", yyyy=str(curr_year + 1)))
     app.dependency_overrides.clear()
 
 
 def test_add_card_succeeds_when_sameCardAlreadyReggdWithAnotherUser(
-    session: Session, client: TestClient, user, admin
+    session: Session, client: TestClient, user, admin, card: CardCreate
 ):
     app.dependency_overrides[deps.get_current_user] = admin
-    client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    client.post("/api/v1/cards", json=jsonable_encoder(card))
 
     app.dependency_overrides[deps.get_current_user] = user
-    response = client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    response = client.post("/api/v1/cards", json=jsonable_encoder(card))
 
     # what it returns
     data = response.json()
     assert response.status_code == 200
-    assert data["number"] == card_1.number
+    assert data["number"] == card.number
 
     # how it registers the new user associated with the card
     card_inDB: Card = session.exec(
-        select(Card).filter(Card.number == utils.util_crypt.encrypt(card_1.number))
+        select(Card).filter(Card.number == utils.util_crypt.encrypt(card.number))
     ).first()
     assert user() in card_inDB.users
     assert admin() in card_inDB.users
@@ -129,14 +138,14 @@ def test_add_card_succeeds_when_sameCardAlreadyReggdWithAnotherUser(
 
 
 def test_get_card_raises404_when_cardExistsNotLoggedUser(
-    client: TestClient, guest, user
+    client: TestClient, guest, user, card: CardCreate
 ):
     app.dependency_overrides[deps.get_current_user] = user
-    client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    client.post("/api/v1/cards", json=jsonable_encoder(card))
 
     app.dependency_overrides[deps.get_current_user] = guest
 
-    response = client.get(f"/api/v1/cards/{card_1.number}")
+    response = client.get(f"/api/v1/cards/{card.number}")
     data = response.json()
     assert response.status_code == 401
     assert data["detail"] == "You should be logged in"
@@ -144,13 +153,13 @@ def test_get_card_raises404_when_cardExistsNotLoggedUser(
 
 
 def test_get_card_returnsCard_when_cardExistsAsssociatedWithUser(
-    client: TestClient, user
+    client: TestClient, user, card: CardCreate
 ):
     app.dependency_overrides[deps.get_current_user] = user
 
-    client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    client.post("/api/v1/cards", json=jsonable_encoder(card))
 
-    response = client.get(f"/api/v1/cards/{card_1.number}")
+    response = client.get(f"/api/v1/cards/{card.number}")
     data = response.json()
 
     assert response.status_code == 200
@@ -161,21 +170,21 @@ def test_get_card_returnsCard_when_cardExistsAsssociatedWithUser(
         holder=data["holder"],
         cvc=data["cvc"],
     )
-    assert found_card.number == card_1.number
-    assert found_card.expiry == card_1.expiry.datetime_
-    assert found_card.holder == card_1.holder
-    assert found_card.cvc == card_1.cvc
+    assert found_card.number == card.number
+    assert found_card.expiry == card.expiry.datetime_
+    assert found_card.holder == card.holder
+    assert found_card.cvc == card.cvc
     app.dependency_overrides.clear()
 
 
 def test_get_card_returnsCard_when_userAdminCardExistsNotAsssociatedWithUser(
-    client: TestClient, admin, user
+    client: TestClient, admin, user, card: CardCreate
 ):
     app.dependency_overrides[deps.get_current_user] = user
-    client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    client.post("/api/v1/cards", json=jsonable_encoder(card))
 
     app.dependency_overrides[deps.get_current_user] = admin
-    response = client.get(f"/api/v1/cards/{card_1.number}")
+    response = client.get(f"/api/v1/cards/{card.number}")
     data = response.json()
 
     assert response.status_code == 200
@@ -186,21 +195,21 @@ def test_get_card_returnsCard_when_userAdminCardExistsNotAsssociatedWithUser(
         holder=data["holder"],
         cvc=data["cvc"],
     )
-    assert found_card.number == card_1.number
-    assert found_card.expiry == card_1.expiry.datetime_
-    assert found_card.holder == card_1.holder
-    assert found_card.cvc == card_1.cvc
+    assert found_card.number == card.number
+    assert found_card.expiry == card.expiry.datetime_
+    assert found_card.holder == card.holder
+    assert found_card.cvc == card.cvc
     app.dependency_overrides.clear()
 
 
 def test_get_card_returns404_when_CardExistsNotAsssociatedWithUser(
-    client: TestClient, admin, user
+    client: TestClient, admin, user, card: CardCreate
 ):
     app.dependency_overrides[deps.get_current_user] = admin
-    client.post("/api/v1/cards", json=jsonable_encoder(card_1))
+    client.post("/api/v1/cards", json=jsonable_encoder(card))
 
     app.dependency_overrides[deps.get_current_user] = user
-    response = client.get(f"/api/v1/cards/{card_1.number}")
+    response = client.get(f"/api/v1/cards/{card.number}")
     data = response.json()
 
     assert response.status_code == 404
