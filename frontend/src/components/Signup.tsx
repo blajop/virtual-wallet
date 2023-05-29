c1740f7dd0d85f33eaf77cecf26428d44297d238
blajop <pavlov_blago@yahoo.com>
2023-05-29 18:36:23 +0300

    Working alert for data taken - to be changed

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,11 +1,10 @@
-import Button from "@mui/base/Button";
-import Alert from "@mui/material/Alert";
-import AlertTitle from "@mui/material/AlertTitle";
-import Collapse from "@mui/material/Collapse";
+// import Button from "@mui/base/Button";
+// import Alert from "@mui/material/Alert";
+// import AlertTitle from "@mui/material/AlertTitle";
+// import Collapse from "@mui/material/Collapse";
 import TextField from "@mui/material/TextField";
 import { Fragment, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 export default function Register() {
   const [form, setForm] = useState({
@@ -16,11 +15,12 @@ export default function Register() {
     phone: "",
     password: "",
   });
+
   const [open, setOpen] = useState(false);
+
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [event.target.name]: event.target.value });
   };
-  const navigate = useNavigate();
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -30,84 +30,86 @@ export default function Register() {
         if (response.status === 200) {
           setOpen(true);
           setTimeout(() => {
-            navigate("/home");
+            // navigate("/home");
           }, 3000);
         }
       })
       .catch((err) => console.log(err));
   }
 
-  return (
-    <Fragment>
-      <form
-        id="form"
-        className="flex justify-center mt-20"
-        onSubmit={handleSubmit}
-      >
-        <div className="w-[350px] justify-center gap-[10px] rounded p-[40px] bg-blue-50 flex flex-col">
-          <TextField
-            required
-            id="outlined-required"
-            name="f_name"
-            label="First name"
-            autoComplete="none"
-            onChange={handleInput}
-          />
-          <TextField
-            required
-            id="outlined-required"
-            name="l_name"
-            label="Last name"
-            autoComplete="none"
-            onChange={handleInput}
-          />
+  return {
+    form,
+    render: (
+      <Fragment>
+        <form
+          id="form"
+          className="flex justify-center mt-20"
+          onSubmit={handleSubmit}
+        >
+          <div className="w-[350px] justify-center gap-[10px] rounded p-[40px] bg-blue-50 flex flex-col">
+            <TextField
+              required
+              id="outlined-required"
+              name="f_name"
+              label="First name"
+              autoComplete="none"
+              onChange={handleInput}
+            />
+            <TextField
+              required
+              id="outlined-required"
+              name="l_name"
+              label="Last name"
+              autoComplete="none"
+              onChange={handleInput}
+            />
 
-          <TextField
-            required
-            id="outlined-required"
-            label="Email"
-            name="email"
-            autoComplete="email"
-            onChange={handleInput}
-          />
-          <TextField
-            required
-            id="outlined-required"
-            label="Phone number"
-            name="phone"
-            autoComplete="phone"
-            onChange={handleInput}
-          />
-        </div>
+            <TextField
+              required
+              id="outlined-required"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              onChange={handleInput}
+            />
+            <TextField
+              required
+              id="outlined-required"
+              label="Phone number"
+              name="phone"
+              autoComplete="phone"
+              onChange={handleInput}
+            />
+          </div>
 
-        <div className="w-[350px] justify-center gap-[10px] rounded p-[40px] bg-blue-50 flex flex-col">
-          <TextField
-            required
-            id="outlined-required"
-            label="Username"
-            name="username"
-            onChange={handleInput}
-          />
+          <div className="w-[350px] justify-center gap-[10px] rounded p-[40px] bg-blue-50 flex flex-col">
+            <TextField
+              required
+              id="outlined-required"
+              label="Username"
+              name="username"
+              onChange={handleInput}
+            />
 
-          <TextField
-            required
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            name="password"
-            autoComplete="new-password"
-            onChange={handleInput}
-          />
+            <TextField
+              required
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              name="password"
+              autoComplete="new-password"
+              onChange={handleInput}
+            />
 
-          <TextField
-            required
-            id="outlined-password-input"
-            label="Confirm password"
-            type="password"
-            autoComplete="new-password"
-          />
-        </div>
-        <div className="mt-20">
+            <TextField
+              required
+              id="outlined-password-input"
+              label="Confirm password"
+              type="password"
+              autoComplete="new-password"
+            />
+          </div>
+          {/* <div className="mt-20">
           <Button
             sx={{ fontWeight: "bold", fontSize: "1rem", mt: "20px" }}
             variant="contained"
@@ -123,8 +125,9 @@ export default function Register() {
               You will now be redirected to the homepage.
             </Alert>
           </Collapse>
-        </div>
-      </form>
-    </Fragment>
-  );
+        </div> */}
+        </form>
+      </Fragment>
+    ),
+  };
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -7,30 +7,41 @@ import Typography from "@mui/material/Typography";
 import { Fragment, ReactNode, useState } from "react";
 import Register from "../components/Signup";
 import Home from "./Home";
+import axios, { AxiosError } from "axios";
+import Collapse from "@mui/material/Collapse";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
 const steps = ["New User", "Two", "Aide"];
-const pages = [<Register />, <Home />];
 
 export default function RegisterStepper() {
+  const { form, render } = Register();
+  const pages = [render, <Home />];
+  const [dataCheck, setDataCheck] = useState(false);
+  const [alert, setAlert] = useState(false);
   const [activeStep, setActiveStep] = useState(0);
   const [activePage, setActivePage] = useState(0);
-  const [skipped, setSkipped] = useState(new Set<number>());
-
-  const isStepSkipped = (step: number) => {
-    return skipped.has(step);
-  };
 
   // NEXT BUTTON HANDLER
   const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
+    if (activeStep === 0) {
+      axios
+        .post("http://localhost:8000/api/v1/data-unique", form)
+        .then((response) => {
+          console.log(response.data);
+          if (response.status === 200) {
+            setDataCheck(true);
+            console.log(form);
+          }
+        })
+        .catch((err: AxiosError) => {
+          console.log(err.response.data);
+          setAlert(true);
+        });
     }
 
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setActivePage((prevActivePage) => prevActivePage + 1);
-    setSkipped(newSkipped);
+    // setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    // setActivePage((prevActivePage) => prevActivePage + 1);
   };
   // BACK BUTTON HANDLER
   const handleBack = () => {
@@ -41,14 +52,11 @@ export default function RegisterStepper() {
   return (
     <Box sx={{ width: "100%" }}>
       <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
+        {steps.map((label) => {
           const stepProps: { completed?: boolean } = {};
           const labelProps: {
             optional?: ReactNode;
           } = {};
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
           return (
             <Step key={label} {...stepProps}>
               <StepLabel {...labelProps}>{label}</StepLabel>
@@ -81,6 +89,13 @@ export default function RegisterStepper() {
               {activeStep === steps.length - 1 ? "Finish" : "Next"}
             </Button>
           </Box>
+          <Collapse in={alert}>
+            <Alert severity="error">
+              <AlertTitle>
+                <strong>Data Taken!</strong>
+              </AlertTitle>
+            </Alert>
+          </Collapse>
         </Fragment>
       )}
     </Box>
