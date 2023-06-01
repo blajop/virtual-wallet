44b404d5247b27f7c83a3bd299de96c686fdedb2
blajop <pavlov_blago@yahoo.com>
2023-06-01 20:21:01 +0300

    CustomAvatar component uploading not visually ok

---
+++ frontend/src/components/Icons/CustomAvatar.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { baseUrl } from "../../shared";
+
+const CustomAvatar = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+  };
+
+  const uploadImage = () => {
+    const formData = new FormData();
+    formData.append("file", selectedImage);
+    axios
+      .post(`${baseUrl}api/v1/users/profile/avatar`, formData, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        console.log("Image uploaded successfully:", response);
+      })
+      .catch((error) => {
+        console.error("Error uploading image:", error);
+      });
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleImageUpload} />
+      <button onClick={uploadImage}>Upload</button>
+    </div>
+  );
+};
+
+export default CustomAvatar;
