af3350983c270f9e370172858923fbd194c18bca
Stanislav Milchev <milchev.st@gmail.com>
2023-06-13 14:47:05 +0000

    Update shared.ts

--- frontend/src/shared.ts
+++ frontend/src/shared.ts
@@ -1,4 +1,4 @@
 // export const baseUrl = 'http://localhost:8000/'
 // export const apiUrl = 'http://localhost:8000/api/v1/'
-export const baseUrl: string = 'https://13.53.168.104/'
-export const apiUrl: string = 'https://13.53.168.104/api/v1/'
+export const baseUrl: string = 'https://uncles-api.onrender.com/'
+export const apiUrl: string = 'https://uncles-api.onrender.com/api/v1/'
